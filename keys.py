6e56a961d0ab24c5ab5fa5a8a3ab4bae6805bfd3
Guang <liewgh1995@gmail.com>
2021-09-25 19:23:31 +0800

    finalized keys

--- keys.py
+++ keys.py
@@ -3,8 +3,7 @@ from os.path import join, dirname
 import os
 
 dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path) # take environment variables from .env.
-# environment variables
+load_dotenv(dotenv_path) # take environment variables from .env
 
 consumer_key= os.environ.get("consumer_key")
 consumer_secret= os.environ.get("consumer_secret")
